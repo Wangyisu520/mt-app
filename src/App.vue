4b22ada59163b66af939aa6fd380c4c3703e55fc
wangyisu <746078001@qq.com>
2019-01-14 15:55:26 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <!-- 头部 -->
+   <app-header :poiInfo="poiInfo"></app-header>
+   <!-- 导航 -->
+   <app-nav :commentNum= 'commentNum'></app-nav>
+   <!-- 内容 -->
+   <keep-alive>
+    <router-view></router-view>
+   </keep-alive>
+  </div>
+</template>
+
+<script>
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+export default {
+  name: "App",
+  components: {
+    "app-header": Header,
+    "app-nav": Nav
+  },
+  data() {
+    return {
+      poiInfo: {},
+      commentNum: 0
+    };
+  },
+  created() {
+    // fetch("/api/goods")
+    //   .then(res =>{
+    //     return res.json()
+    //   })
+    //   .then(response =>{
+    //     // console.log(response)
+    //     if(response.code == 0){
+    //       this.poiInfo = response.data.poi_info
+    //       // console.log(this.poiInfo)
+    //     }
+    //   })
+
+    // fetch("/api/ratings")
+    //   .then(res => {
+    //     return res.json();
+    //   })
+    //   .then(response => {
+    //     console.log(response.data);
+    //     this.commentNum = response.data.comment_num;
+    //   });
+
+    fetch("https://wd2151358536yxwfcu.wilddogio.com/data.json")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        // console.log(data)
+        this.poiInfo = data.poi_info;
+      });
+
+    fetch("https://wd6572388885nxklda.wilddogio.com/data.json")
+      .then(res => {
+        return res.json();
+      })
+      .then(response => {
+        // console.log(response);
+        this.commentNum = response.comment_num;
+      });
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/star/Star.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="star">
+        <span class="star-item" v-for="(itemClass,index) in itemClass" :key="index" :class="itemClass"></span>
+    </div>
+</template>
+ 
+<script>
+const LENGTH = 5;
+//星星状态类名
+const CLS_ON = "on";
+const CLS_HALF = "half";
+const CLS_OFF = "off";
+export default {
+  props: {
+    score: {
+      type: Number
+    }
+  },
+  computed: {
+    itemClass() {
+      let result = [];
+
+      let score = Math.floor(this.score * 2) / 2;
+        //半星
+      let hasDecimal = score % 1 !== 0;
+        //全星
+      let integer = Math.floor(score);
+
+      for (let i = 0; i < integer; i++) {
+        result.push(CLS_ON);
+      }
+      if (hasDecimal) {
+        result.push(CLS_HALF);
+      }
+      while (result.length < LENGTH) {
+        result.push(CLS_OFF);
+      }
+      return result;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.star {
+  font-size: 0;
+}
+.star .star-item {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  margin-right: 3px;
+  background-repeat: no-repeat;
+  background-size: 10px 10px;
+}
+.star .star-item:last-child {
+  margin-right: 0;
+}
+
+.star .on {
+  background-image: url(img/star24_on@2x.png);
+}
+.star .half {
+  background-image: url(img/star24_half@2x.png);
+}
+.star .off {
+  background-image: url(img/star24_off@2x.png);
+}
+</style>
